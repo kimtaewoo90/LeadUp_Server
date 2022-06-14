60c65af616e6dcff204e11fa323255de84a0403c
kimtaewoo90 <52480205+kimtaewoo90@users.noreply.github.com>
2022-06-14 22:08:40 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,120 +1,190 @@
+# -*- coding:utf-8 -*-
+
 import sqlite3
-from flask import Flask, url_for, render_template, request, redirect, session, flash, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
+from flask import Flask, url_for, render_template, request, redirect, session, flash
 
 import pandas as pd
+import numpy as np
+import sys
+from datetime import datetime
 
+# upbit modules
+import modules.upbit as upbit
+
+# web sockets
+import upbit_websockets as ws
+import asyncio
+import logging
+import traceback
+
+# schedule
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.secret_key = "234231432423"
-app.config['LEADUP_USER_DB'] = 'db/user_db.sqlite3'
+app.config['CoinGo_DB'] = 'db/user_db.sqlite3'
 
+# RSI Strategy
+# initial RSI
 
-@app.route("/")
-def home():
-    if session.get("logged_in"):
-        return render_template('home.html')
-    else:
-        return render_template('home.html')
+# rsi_dict = dict()
+def calcRSI(market_name):
     
+    rsi_dict = dict()
     
-    
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        user_id = request.form['user_id']
-        user_pw = request.form['user_pw']
+    for i in range(len(market_name)):
+        candle_datas = upbit.get_candle(market_name[i], '60', 200)
         
-        conn = sqlite3.connect(app.config["LEADUP_USER_DB"])
-        
-        user_info = pd.read_sql_query("""SELECT * 
-                                            FROM user_info 
-                                            WHERE user_id = '{}'""".format(user_id), conn)
-        
-        if len(user_info) == 0:
-            return {"message" : "failed to login",
-                    "error" : "not exist user id"}, 500
+        rsi_df = upbit.get_candle_rsi(candle_datas)
+        # if i == 0:
+        #     print("{0} RSI : {1}".format(market_name[i], rsi_df))
             
+        rsi_dict[market_name[i]] = rsi_df
+    
+    return rsi_dict
+
+
+def real_time_RSI(close_price_list, current_price, period=14):
+    
+    close_price_sr = pd.Series(close_price_list.append(current_price))
+    
+    U = np.where(close_price_sr.diff(1) > 0, close_price_sr.diff(1), 0)
+    D = np.where(close_price_sr.diff(1) < 0, close_price_sr.diff(1), 0)
+    
+    AU = pd.Series(U).rolling(window=period).mean()
+    AD = pd.Series(D).rolling(window=period).mean()
+    
+    RSI = AU / (AD + AU) * 100
+    
+    return RSI
+
+# Current Price
+
+# Balance
+def available_balance():
+    return upbit.get_krwbal()
+
+
+# Initial set up
+def init_setup():
+    item_list = upbit.get_items("KRW")
+    market_name = []
+    market_kr_name = []
+    
+    for market in range(len(item_list)):
+        market_name.append(item_list[market]['market'])
+        market_kr_name.append(item_list[market]['korean_name'])
         
-        if user_pw != user_info.user_pw[0]:
-            return {"message" : "failed to login",
-                    "error" : "didn't match password"}, 500
+    return market_name, market_kr_name
+    
+
+# TODO: WebSocket 이랑 RSI 계산 및 주문 등은 Thread를 따로 줘야겠다.
+@app.route('/')
+def home():
+    
+    # strategy
+    first_touch_30 = []
+    second_touch_30 = []  # buy
+    first_touch_70 = []
+    second_touch_70 = []  # sell
+    
+    market_name, market_kr_name = init_setup()
+    rsi_df = calcRSI(market_name)
+    
+    try_again = True
+    once_in_hour = True
+    
+    
+    try:
+        # ---------------------------------------------------------------------
+        # Logic Start!
+        # ---------------------------------------------------------------------
+        # 웹소켓 시작
+        # asyncio.run(ws.main(market_name))
         
-        if len(user_info) == 1:
-            # check pw
-            if user_pw == user_info.user_pw[0]:
-                session['username'] = user_info.user_nickname[0]
-                session['logged_in'] = True
+        # calculate realtime rsi during every 1 hour (60 mins candle)
+        # TODO: 60분봉 마지막 price를 웹소켓을 통해 받은 현재가를 사용하여 RSI 계속 계산.
+        
+        while(True):
+            
+            # after 60 mins
+            if datetime.now().minute == 0 and once_in_hour is True:
+                rsi_df = calcRSI(market_name)
+                try_again = True
+                once_in_hour = False
+            
+            if datetime.now().minute == 1:
+                once_in_hour = True    
+            
+            if try_again is True:
+                try_again = False
                 
-                return jsonify(
-                    message = "success",
-                    access_token = create_access_token(identify=user_id, elxpires_delta = False)
-                )
-
-
-@app.route("/logout", methods=["POST","GET"])
-def logout():
-    if request.method == "POST":
-        session['logged_in'] = False
-        session.pop('username')
+                for i in range(len(market_name)):
+                    rsi = rsi_df[market_name[i]].iloc[-1]["RSI"]
+                    times = rsi_df[market_name[i]].iloc[-1]["candle_date_time_kst"]
+                    ticker = market_name[i]
+                    
+                    if ticker not in first_touch_30:
+                        if rsi < 30:
+                            first_touch_30.append(ticker)
+                            if ticker in second_touch_70:
+                                second_touch_70.remove(ticker)
+                                
+                    elif ticker in first_touch_30:
+                        if rsi > 30 and rsi < 38:
+                            # 매수
+                            second_touch_30.append(ticker)
+                            first_touch_30.remove(ticker)
+                            print("Ticker : {0}, Buy_price : {1}".format(ticker, rsi_df[ticker].iloc[-1]["close"]))
+                            
+                    
+                    elif ticker in second_touch_30 and ticker not in first_touch_70:
+                        if rsi > 70:
+                            first_touch_70.append(ticker)
+                            second_touch_30.remove(ticker)
+                            
+                    elif market_name[i] in first_touch_70:
+                        if rsi < 70:
+                            # 매도
+                            second_touch_70.append(ticker)
+                            first_touch_70.remove(ticker)
+                            print("Ticker : {0}, Sell_price : {1}".format(ticker, rsi_df[ticker].iloc[-1]["close"]))
+                            
+                print("[{0}] : First_30 = {1}".format(times, first_touch_30))
+                print("[{0}] : Second_30 = {1}".format(times, second_touch_30))
+                print("[{0}] : First_70 = {1}".format(times, first_touch_70))
+                print("[{0}] : Second_30 = {1}".format(times, second_touch_70))
+                print("==" * 30)
+                    
+            
+            # print(datetime.now().minute)
         
-        return render_template('home.html')
+        
+        
+        
+        
     
-    else:
-        return render_template('home.html')
-
-
-
-@app.route("/register", methods=["POST","GET"])
-def register():
-    if request.method == "GET":
-        return render_template('register.html')
+    except KeyboardInterrupt:
+        logging.error("KeyboardInterrupt Exception 발생!")
+        logging.error(traceback.format_exc())
+        sys.exit(-100)
+ 
+    except Exception:
+        logging.error("Exception 발생!")
+        logging.error(traceback.format_exc())
+        sys.exit(-200)
     
-    elif request.method == "POST":
-        
-        user_id = request.form['user_id']
-        user_password = request.form['user_password']
-        user_email = request.form['user_email']
-        user_nickname = request.form['user_nickname']
-        user_phone_number = request.form['user_phone_number']
-        
-        print(request.form)
-        
-        conn = sqlite3.connect(app.config['LEADUP_USER_DB'])
-        
-        check_duplicate_phone_number = pd.read_sql_query("""SELECT user_id, user_phone_number 
-                                                FROM user_info 
-                                                WHERE user_phone_number = '{}'""".format(user_phone_number), conn)
-        
-        
-        check_duplicate_id = pd.read_sql_query("""SELECT user_id, user_phone_number 
-                                                FROM user_info 
-                                                WHERE user_id = '{}'""".format(user_id), conn)
-        
-        conn.close()
-        
-        if len(check_duplicate_phone_number) >= 1:
-            return {"message" : "register failed",
-                    "error" : "already exist"}, 500
-        
-        if len(check_duplicate_id) >= 1:
-            return {"message" : "register failed",
-                    "error" : "duplicated ID"}, 500
-        
-        
-        datas = [user_id, user_password, user_nickname, user_email, user_phone_number]
-        conn = sqlite3.connect(app.config['LEADUP_USER_DB'])
-        cursor = conn.cursor()
-        cursor.execute("""INSERT INTO user_info VALUES (?, ?, ?, ?, ?)
-                     """, datas)
-        
-        conn.commit()
-        conn.close()
-        
-        return {"message" : "success"}, 200
-        
+    return render_template('index.html')
+    
+    
+
+@app.route('/get_items')
+def get_items():
+    
+    return upbit.get_items("KRW")
+
 
 
 if __name__ == "__main__":
+    
     app.run(debug=True)
